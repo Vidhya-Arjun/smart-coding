d52b6490b06065c9109790b363277c1e61270da9
Vidhya-Arjun <53186437+Vidhya-Arjun@users.noreply.github.com>
2019-07-23 21:44:50 +0200

    Create basic.py

---
+++ python/basic.py
@@ -0,0 +1,36 @@
+# multiply a number with 3 using function
+
+
+def mult(number):
+   result = 3*number;
+   return result;
+   
+number_mul = int(input("Enter the value to be multiplied "));
+print("The product value is ", mult(number_mul)) 
+
+
+## addition of 2 numbers using function
+
+
+def add(a,b):
+   addition_of_num = a + b;
+   return addition_of_num ;
+   
+number_1 = int(input("Enter the first value to be added "));
+number_2 = int(input("Enter the second value to be added "));=
+print("The added value is ", add(number_1,number_2))
+
+
+
+#code to display position and sum of list
+
+numbers = [1,2,3,6]
+sum = 0
+def sumOfListNumbers(someList):   
+val_get = someList;
+ return val_get;
+for val_get in numbers:
+  print(val_get)
+sum += val_get  
+ print(sum)
+
